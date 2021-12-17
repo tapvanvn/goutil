855b53c996cd815b5b4e489f6b77ddeb242321df
Duy Nguyen <tapvanvn@gmail.com>
2021-12-18 01:55:53 +0700

    fix md5

--- hash.go
+++ hash.go
@@ -2,12 +2,12 @@ package goutil
 
 import (
 	"crypto/md5"
-
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"encoding/hex"
 )
 
 //MD5 md5 message to footprint
 func MD5Message(msg string) string {
 	hasher := md5.New()
-	return hexutil.Encode(hasher.Sum([]byte(msg)))[2:]
+	hasher.Write([]byte(msg))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
