e2f49fd7f13589714d323828ce31ee76ae87af2d
Duy Nguyen <tapvanvn@gmail.com>
2022-09-10 14:54:44 +0700

    update

--- generate.go
+++ generate.go
@@ -1,12 +1,17 @@
 package goutil
 
 import (
+	"bytes"
+	"crypto/hmac"
 	crypto_rand "crypto/rand"
+	"crypto/sha1"
 	"encoding/base32"
 	"encoding/binary"
 	"log"
 	"math/rand"
+	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -127,4 +132,55 @@ func Gen2FASecret() string {
 		panic("cannot seed math/rand package with cryptographically secure random number generator")
 	}
 	return base32.StdEncoding.EncodeToString(b[:])
+}
+
+//MARK: 2FA - from https://github.com/tilaklodha/google-authenticator
+func Prefix2FAOTP(otp string) string {
+	if len(otp) == 6 {
+		return otp
+	}
+	for i := (6 - len(otp)); i > 0; i-- {
+		otp = "0" + otp
+	}
+	return otp
+}
+func Get2FAOTP(secret string) (string, error) {
+
+	interval := time.Now().Unix() / 30
+	//Converts secret to base32 Encoding. Base32 encoding desires a 32-character
+	//subset of the twenty-six letters A–Z and ten digits 0–9
+	key, err := base32.StdEncoding.DecodeString(strings.ToUpper(secret))
+	if err != nil {
+		return "", err
+	}
+	bs := make([]byte, 8)
+	binary.BigEndian.PutUint64(bs, uint64(interval))
+
+	//Signing the value using HMAC-SHA1 Algorithm
+	hash := hmac.New(sha1.New, key)
+	hash.Write(bs)
+	h := hash.Sum(nil)
+
+	// We're going to use a subset of the generated hash.
+	// Using the last nibble (half-byte) to choose the index to start from.
+	// This number is always appropriate as it's maximum decimal 15, the hash will
+	// have the maximum index 19 (20 bytes of SHA1) and we need 4 bytes.
+	o := (h[19] & 15)
+
+	var header uint32
+	//Get 32 bit chunk from hash starting at the o
+	r := bytes.NewReader(h[o : o+4])
+	err = binary.Read(r, binary.BigEndian, &header)
+
+	if err != nil {
+		return "", err
+	}
+	//Ignore most significant bits as per RFC 4226.
+	//Takes division from one million to generate a remainder less than < 7 digits
+	h12 := (int(header) & 0x7fffffff) % 1000000
+
+	//Converts number as a string
+	otp := strconv.Itoa(int(h12))
+
+	return Prefix2FAOTP(otp), nil
 }
